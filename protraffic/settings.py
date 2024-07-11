b9c72daa2037e879d5ea7ab07b758160d9242dd4
atul <atulg0736@gmail.com>
2024-07-11 19:30:09 +0530

    db

--- protraffic/settings.py
+++ protraffic/settings.py
@@ -64,23 +64,23 @@ WSGI_APPLICATION = 'protraffic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE':'django.db.backends.mysql',
-        'NAME': 'rbugtige_Traffic',
-        'USER': 'rbugtige_Traffic',
-        'PASSWORD': 'Kyppma2PHCnbZK2MGacq',
-        'HOST': 'lithium.scnservers.net',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE':'django.db.backends.mysql',
+#         'NAME': 'rbugtige_Traffic',
+#         'USER': 'rbugtige_Traffic',
+#         'PASSWORD': 'Kyppma2PHCnbZK2MGacq',
+#         'HOST': 'lithium.scnservers.net',
+#         'PORT':'3306',
+#     }
+# }
 
 
 
